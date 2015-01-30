91f3874b549f1009d29d0db35cda152e050e6f40
rickydo <g3tdo@yahoo.com>
2015-01-29 19:12:34 -0600

    added todo controller and restful routes using resource

--- laravel-basics/app/routes.php
+++ laravel-basics/app/routes.php
@@ -11,17 +11,24 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('todos.index');
-});
-Route::get('todos', function()
-{
-	return View::make('todos/index');
-});
+Route::resource('todos', 'TodoListController');
+
+// Route::get('/', 'TodoListController@index');
+// Route::get('todos', 'TodoListController@index');
+// Route::get('todos/{id}', 'TodoListController@show');
 
 
-Route::get('todos/{id}', function($id)
-{
-	return View::make('todos.show')->withId($id);
-});
+// / = home
+// /todos -> all lists
+// /todos/1 -> show
+// /todos/1/edit - edit and update
+// /todos/create - create new list
+
+
+// to bring up the current routes
+// homestead ssh->Sites->laravel basics-> php artisan routes
+
+// to bring up the artisan commands 
+// php artisan help
+
+
